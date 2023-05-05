3430acbecd02467b5771fef2e4442ca0c23568bc
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2023-05-05 21:36:44 +0300

    add project

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const widgetButton = document.querySelector('.widget-button');
+const widgetDescription = document.querySelector('.widget-description');
+
+
+widgetButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    showText();
+});
+
+function showText() {
+    console.log(widgetDescription);
+    widgetDescription.classList.toggle('widget-description__discovery');
+}
