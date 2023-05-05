0a1714f387ef49e8ad520abb13f6ed547f199b06
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2023-05-05 21:40:51 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
-const widgetButton = document.querySelector('.widget-button');
-const widgetDescription = document.querySelector('.widget-description');
+const widgetButton = document.querySelector(".widget-button");
+const widgetDescription = document.querySelector(".widget-description");
 
-
-widgetButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    showText();
+widgetButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  showText();
 });
 
 function showText() {
-    console.log(widgetDescription);
-    widgetDescription.classList.toggle('widget-description__discovery');
+  console.log(widgetDescription);
+  widgetDescription.classList.toggle("widget-description__discovery");
 }
